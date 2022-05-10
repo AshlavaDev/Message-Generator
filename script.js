52520f894ace91d92d929be5d5299cb8055a87c3
AshlavaDev <ashleymorgan19901023@gmail.com>
2022-05-10 15:17:40 -0600

    Created script and printed test message

---
+++ script.js
@@ -0,0 +1,18 @@
+//A script that prints out a randomly created message when run.
+
+const testMessage = "This is a test";
+
+
+
+
+
+
+
+function printMessage(message) {
+    console.log(message);
+}
+
+
+
+printMessage(testMessage);
+
