2a651d388b3c248dd05abe7e7ac91caa69ef2ec2
AshlavaDev <ashleymorgan19901023@gmail.com>
2022-05-10 18:59:56 -0600

    Added function to get a random element from a list

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 //A script that prints out a randomly created message when run.
 
 //const testMessage = "This is a test";
+
+//Lists for the different components of the message to be randomized
 const subjects = ["Death", "Light", "Skulls", "Blood", "Purity", "Truth"];
 const verbs = ["becomes", "eats", "forgets", "for", "dances with", "plays with"];
 const objects = ["you", "him", "them", "her", "it", "the Skull Throne", "the Blood God"];
 
+//A function that picks a random element from a list
+function getElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
 
+
+
+//The function that creates the message
+function message() {
+
+}
 
 
 
@@ -15,5 +27,5 @@ function printMessage(message) {
 
 
 
-//printMessage(testMessage);
+//printMessage(getElement(subjects));
 
